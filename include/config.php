9b7dfcde4ca14185732073555ab019670d6ed753
Mile Janev <Mile Janev@Mj-laptop>
2015-03-07 12:58:52 +0100

    DB config

--- include/config.php
+++ include/config.php
@@ -4,7 +4,7 @@
  * Database config variables
  */
 define("DB_HOST", "localhost");
-define("DB_USER", "root");
-define("DB_PASSWORD", "");
-define("DB_DATABASE", "android_api");
+define("DB_USER", "radovis");
+define("DB_PASSWORD", "ypwU5k39");
+define("DB_DATABASE", "radovis9");
 ?>
